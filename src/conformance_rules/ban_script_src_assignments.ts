41c641b02b85af6af9ef651bdab72384f6cf66c1
siegrift <siegrift@google.com>
2020-09-23 07:45:52 -0700

    Enable type awareness in DOM security rules which are based on ConformancePatternRule for open source.
    PiperOrigin-RevId: 333290325

--- src/conformance_rules/ban_script_src_assignments.ts
+++ src/conformance_rules/ban_script_src_assignments.ts
@@ -14,6 +14,8 @@
 
 import {ConformancePatternRule, ErrorCode, PatternKind} from '../third_party/tsetse/rules/conformance_pattern_rule';
 import {AllowlistEntry} from '../third_party/tsetse/util/allowlist';
+import {overridePatternConfig} from '../third_party/tsetse/util/pattern_config';
+import {TRUSTED_SCRIPT_URL} from '../third_party/tsetse/util/trusted_types_configuration';
 
 let errMsg =
     'Do not assign variables to HTMLScriptElement#src, as this can lead to XSS.';
@@ -27,14 +29,15 @@ export class Rule extends ConformancePatternRule {
 
   constructor(allowlistEntries?: AllowlistEntry[]) {
     super(
-        {
+        overridePatternConfig({
           errorCode: ErrorCode.CONFORMANCE_PATTERN,
           errorMessage: errMsg,
           kind: PatternKind.BANNED_PROPERTY_WRITE,
           values: ['HTMLScriptElement.prototype.src'],
           allowlistEntries,
           name: Rule.RULE_NAME,
-        },
+          allowedTrustedType: TRUSTED_SCRIPT_URL,
+        }),
     );
   }
 }
