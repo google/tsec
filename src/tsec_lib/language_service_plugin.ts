d5b520db47c1287b82d43d7b0e6631aff4bb3b47
siegrift <siegrift@google.com>
2020-10-05 06:45:12 -0700

    Integrate fixers with TS code action API
    PiperOrigin-RevId: 335406454

--- src/tsec_lib/language_service_plugin.ts
+++ src/tsec_lib/language_service_plugin.ts
@@ -18,6 +18,8 @@
  */
 
 import {Checker} from '../third_party/tsetse/checker';
+import {ErrorCode} from '../third_party/tsetse/error_code';
+import {DiagnosticWithFix} from '../third_party/tsetse/failure';
 import * as ts from 'typescript/lib/tsserverlibrary';
 
 import {ENABLED_RULES} from '../rule_groups';
@@ -39,24 +41,61 @@ function createProxy<T>(delegate: T): T {
   return proxy;
 }
 
+function diagnosticToCodeFixAction(d: DiagnosticWithFix): ts.CodeFixAction|
+    undefined {
+  if (!d.fix) return undefined;
+
+  return {
+    fixName: 'Tsec fix',            // for TS telemetry use only
+    description: 'Apply tsec fix',  // display name of the code action shown
+                                    // in the IDE
+    changes:
+        d.fix.changes.map(c => ({
+                            fileName: c.sourceFile.fileName,
+                            textChanges: [{
+                              span: {start: c.start, length: c.end - c.start},
+                              newText: c.replacement
+                            }]
+                          }))
+  };
+}
+
+function computeKey(start: number, end: number): string {
+  return `[${start},${end}]`;
+}
+
+/* @internal */
+// Work around for missing API to register a code fix.
+interface CodeFix {
+  errorCodes: number[];
+  getCodeActions(context: unknown): ts.CodeAction[]|undefined;
+}
+
+interface TsWithCodefix {
+  codefix: {registerCodeFix: (codefix: CodeFix) => void};
+}
 
 /**
- * Installs the Tsec language server plugin, which checks Tsec rules in your
- * editor and shows issues as semantic errors (red squiggly underline).
+ * Implementation of the tsec LSP.
+ *
+ * Instance methods of this class are used as references and they need to
+ * bound `this`. This is the reason why we use arrow functions for the property
+ * methods.
  */
-function init(): ts.server.PluginModule {
-  return {
-    create(info: ts.server.PluginCreateInfo) {
-      const oldService = info.languageService;
-      const proxy = createProxy(oldService);
+class TsecLanguageServicePlugin {
+  codeFixActions: Map<string, Map<string, ts.CodeFixAction[]>>;
+  oldService: ts.LanguageService;
 
-      // Note that this ignores suggested fixes. Fixes can be implemented in a
-      // separate proxy method. See:
-      // https://github.com/angelozerr/tslint-language-service/blob/880486c5f1db7961fb7170a621e25893332b2430/src/index.ts#L415
-      proxy.getSemanticDiagnostics = (fileName: string) => {
-        const result = [...oldService.getSemanticDiagnostics(fileName)];
+  constructor(readonly info: ts.server.PluginCreateInfo) {
+    // to avoid running tsetse twice, we store the code actions while running
+    // the checker on `getSemanticDiagnostics`.
+    this.codeFixActions = new Map();
+    this.oldService = info.languageService;
+  }
 
-        const program = oldService.getProgram();
+  boundGetSemanticDiagnostics =
+      (fileName: string) => {
+        const program = this.oldService.getProgram();
         if (!program) {
           throw new Error(
               'Failed to initialize tsetse language_service_plugin: program is undefined',
@@ -70,15 +109,80 @@ function init(): ts.server.PluginModule {
           new rule().register(checker);
         }
 
+        const failures = checker.execute(program.getSourceFile(fileName)!);
+
+        this.codeFixActions.set(fileName, new Map());
+        const codeActionsForCurrentFile = this.codeFixActions.get(fileName)!;
+        for (const failure of failures) {
+          const d = failure.toDiagnostic();
+          const codeAction = diagnosticToCodeFixAction(d);
+          if (codeAction) {
+            // ts.Diagnostic#start is optional, but should always be defined
+            // when used from failure.
+            const key = computeKey(d.start!, d.end);
+            if (!codeActionsForCurrentFile.has(key)) {
+              codeActionsForCurrentFile.set(key, []);
+            }
+            codeActionsForCurrentFile.get(key)!.push(codeAction);
+          }
+        }
+
+        // make sure to mutate the array and not create a copy
+        // otherwise the code fixes stop working
+        const result = this.oldService.getSemanticDiagnostics(fileName);
         result.push(
             ...checker.execute(program.getSourceFile(fileName)!)
-                .map((failure) => {
-                  // Show the diagnostic with stringigied fix (if there is any).
-                  return failure.toDiagnosticWithStringifiedFix();
-                }),
+                .map((failure) => failure.toDiagnostic()),
         );
         return result;
-      };
+      }
+
+  boundGetCodeFixesAtPosition =
+      (fileName: string, start: number, end: number, errorCodes: number[],
+       formatOptions: ts.FormatCodeSettings,
+       userPreferences: ts.UserPreferences): readonly ts.CodeFixAction[] => {
+        const prior = this.oldService.getCodeFixesAtPosition(
+            fileName, start, end, errorCodes, formatOptions, userPreferences);
+        const fixes = [...prior];
+
+        const codeActionsForCurrentFile = this.codeFixActions.get(fileName);
+        if (codeActionsForCurrentFile) {
+          const actions =
+              codeActionsForCurrentFile.get(computeKey(start, end)) ?? [];
+
+          fixes.push(...actions);
+        }
+
+
+        return fixes;
+      }
+}
+
+/**
+ * Installs the Tsec language server plugin, which checks Tsec
+ * rules in your editor and shows issues as semantic errors
+ * (red squiggly underline).
+ */
+function init(modules: {typescript: typeof ts}): ts.server.PluginModule {
+  const tsModule = modules.typescript;
+
+  // every error code tsec produces must be registered
+  // using the internal codefix API
+  if ((tsModule as unknown as TsWithCodefix).codefix) {
+    (tsModule as unknown as TsWithCodefix).codefix.registerCodeFix({
+      errorCodes: [ErrorCode.CONFORMANCE_PATTERN],
+      getCodeActions: () => undefined
+    });
+  }
+
+  return {
+    create(info: ts.server.PluginCreateInfo) {
+      const plugin = new TsecLanguageServicePlugin(info);
+      const oldService = info.languageService;
+      const proxy = createProxy(oldService);
+
+      proxy.getSemanticDiagnostics = plugin.boundGetSemanticDiagnostics;
+      proxy.getCodeFixesAtPosition = plugin.boundGetCodeFixesAtPosition;
 
       return proxy;
     }
