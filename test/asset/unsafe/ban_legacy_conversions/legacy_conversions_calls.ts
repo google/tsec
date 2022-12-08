33e283b6024bfc1b16450d8b6ab6ba49570dc317
ISE Hardening <noreply@google.com>
2022-12-08 04:05:50 -0800

    N/A
    PiperOrigin-RevId: 493853733
    Change-Id: I6c532b4c7d09f93861b77aae6c9f20a27fbcc1b8

--- test/asset/unsafe/ban_legacy_conversions/legacy_conversions_calls.ts
+++ test/asset/unsafe/ban_legacy_conversions/legacy_conversions_calls.ts
@@ -1,23 +1,12 @@
-import * as legacyConversion from 'safevalues/unsafe/legacy';
-import {legacyConversionToHtml, legacyConversionToScript, legacyConversionToScriptUrl} from 'safevalues/unsafe/legacy';
-import * as legacyConversion2 from 'safevalues/restricted/legacy';
-import {legacyUnsafeHtml as legacyConversionToHtml2, legacyUnsafeResourceUrl as legacyConversionToScriptUrl2, legacyUnsafeScript as legacyConversionToScript2} from 'safevalues/restricted/legacy';
+import * as legacy from 'safevalues/restricted/legacy';
+import {legacyUnsafeHtml, legacyUnsafeResourceUrl, legacyUnsafeScript} from 'safevalues/restricted/legacy';
 
 declare var unsafeValue: string;
 
-legacyConversionToHtml(unsafeValue);
-legacyConversionToScriptUrl(unsafeValue);
-legacyConversionToScript(unsafeValue);
+legacyUnsafeHtml(unsafeValue);
+legacyUnsafeResourceUrl(unsafeValue);
+legacyUnsafeScript(unsafeValue);
 
-legacyConversion.legacyConversionToHtml(unsafeValue);
-legacyConversion.legacyConversionToScriptUrl(unsafeValue);
-legacyConversion.legacyConversionToScript(unsafeValue);
-
-legacyConversionToHtml2(unsafeValue);
-legacyConversionToScriptUrl2(unsafeValue);
-legacyConversionToScript2(unsafeValue);
-
-legacyConversion2.legacyUnsafeHtml(unsafeValue);
-legacyConversion2.legacyUnsafeResourceUrl(unsafeValue);
-legacyConversion2.legacyUnsafeScript(unsafeValue);
-
+legacy.legacyUnsafeHtml(unsafeValue);
+legacy.legacyUnsafeResourceUrl(unsafeValue);
+legacy.legacyUnsafeScript(unsafeValue);
--- test/asset/unsafe/ban_reviewed_conversions/unchecked_conversions_calls.ts
+++ test/asset/unsafe/ban_reviewed_conversions/unchecked_conversions_calls.ts
@@ -1,27 +1,12 @@
-import * as uncheckedConversion from 'safevalues/unsafe/reviewed';
-import {htmlFromStringKnownToSatisfyTypeContract, scriptFromStringKnownToSatisfyTypeContract, scriptUrlFromStringKnownToSatisfyTypeContract} from 'safevalues/unsafe/reviewed';
-import * as uncheckedConversion2 from 'safevalues/restricted/reviewed';
-import {htmlSafeByReview as htmlFromStringKnownToSatisfyTypeContract2, resourceUrlSafeByReview as scriptUrlFromStringKnownToSatisfyTypeContract2, scriptSafeByReview as scriptFromStringKnownToSatisfyTypeContract2} from 'safevalues/restricted/reviewed';
+import * as reviewed from 'safevalues/restricted/reviewed';
+import {htmlSafeByReview, resourceUrlSafeByReview, scriptSafeByReview} from 'safevalues/restricted/reviewed';
 
 declare var unsafeValue: string;
 
-htmlFromStringKnownToSatisfyTypeContract(unsafeValue, 'for testing');
-scriptUrlFromStringKnownToSatisfyTypeContract(
-    unsafeValue, 'for testing');
-scriptFromStringKnownToSatisfyTypeContract(unsafeValue, 'for testing');
+htmlSafeByReview(unsafeValue, 'for testing');
+resourceUrlSafeByReview(unsafeValue, 'for testing');
+scriptSafeByReview(unsafeValue, 'for testing');
 
-uncheckedConversion.htmlFromStringKnownToSatisfyTypeContract(
-    unsafeValue, 'for testing');
-uncheckedConversion.scriptUrlFromStringKnownToSatisfyTypeContract(
-    unsafeValue, 'for testing');
-uncheckedConversion.scriptFromStringKnownToSatisfyTypeContract(
-    unsafeValue, 'for testing');
-
-htmlFromStringKnownToSatisfyTypeContract2(unsafeValue, 'for testing');
-scriptUrlFromStringKnownToSatisfyTypeContract2(
-    unsafeValue, 'for testing');
-scriptFromStringKnownToSatisfyTypeContract2(unsafeValue, 'for testing');
-
-uncheckedConversion2.htmlSafeByReview(unsafeValue, 'for testing');
-uncheckedConversion2.resourceUrlSafeByReview(unsafeValue, 'for testing');
-uncheckedConversion2.scriptSafeByReview(unsafeValue, 'for testing');
+reviewed.htmlSafeByReview(unsafeValue, 'for testing');
+reviewed.resourceUrlSafeByReview(unsafeValue, 'for testing');
+reviewed.scriptSafeByReview(unsafeValue, 'for testing');
