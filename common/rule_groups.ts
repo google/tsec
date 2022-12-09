7f916f8079bd2b80f03685682a7f70a0aa3a4019
Pei Wang <pwng@google.com>
2022-12-08 17:23:38 -0800

    Fix broken tests.
    PiperOrigin-RevId: 494036743
    Change-Id: Ifacd9fdf6d97e1a976a55d4c1103899b93a4a670

--- common/rule_groups.ts
+++ common/rule_groups.ts
@@ -27,7 +27,7 @@ import {Rule as TTBanElementSetAttribute} from './rules/dom_security/ban_element
 import {Rule as TTBanEvalCalls} from './rules/dom_security/ban_eval_calls';
 import {Rule as TTBanFunctionCalls} from './rules/dom_security/ban_function_calls';
 import {Rule as TTBanIFrameSrcdocAssignments} from './rules/dom_security/ban_iframe_srcdoc_assignments';
-import {Rule as DomBanObjectDataAssignments} from './rules/dom_security/ban_object_data_assignments';
+import {Rule as TTBanObjectDataAssignments} from './rules/dom_security/ban_object_data_assignments';
 import {Rule as TTBanRangeCreateContextualFragment} from './rules/dom_security/ban_range_createcontextualfragment';
 import {Rule as TTBanScriptAppendChildCalls} from './rules/dom_security/ban_script_appendchild_calls';
 import {Rule as TTBanScriptContentAssignments} from './rules/dom_security/ban_script_content_assignments';
@@ -60,6 +60,7 @@ export const TRUSTED_TYPES_RELATED_RULES: readonly RuleConstructor[] = [
   TTBanEvalCalls,
   TTBanFunctionCalls,
   TTBanIFrameSrcdocAssignments,
+  TTBanObjectDataAssignments,
   TTBanScriptAppendChildCalls,
   TTBanScriptContentAssignments,
   TTBanScriptSrcAssignments,
