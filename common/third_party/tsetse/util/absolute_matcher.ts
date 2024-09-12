044f90d2a0635c823884b59d22f683b74bad6a6a
Laura Harker <lharker@google.com>
2024-09-12 15:58:41 -0700

    No public description
    PiperOrigin-RevId: 674044072
    Change-Id: I25c46328340e8e560f46bb25e5c57ebc53e0b3b4

--- common/third_party/tsetse/util/absolute_matcher.ts
+++ common/third_party/tsetse/util/absolute_matcher.ts
@@ -156,9 +156,13 @@ export class AbsoluteMatcher {
     debugLog(() => `got FQN ${fqn}`);
 
     // Name-based check: `getFullyQualifiedName` returns `"filename".foo.bar` or
-    // just `foo.bar` if the symbol is ambient. The check here should consider
-    // both cases.
-    if (!fqn.endsWith('".' + this.bannedName) && fqn !== this.bannedName) {
+    // just `foo.bar` or `global.foo.bar` if the symbol is ambient. The check
+    // here should consider all three cases.
+    if (
+      !fqn.endsWith('".' + this.bannedName) &&
+      fqn !== this.bannedName &&
+      fqn !== 'global.' + this.bannedName
+    ) {
       debugLog(() => `FQN ${fqn} doesn't match name ${this.bannedName}`);
       return false;
     }
