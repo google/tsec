dbab43234dc8fdc1637b205143f2588df901076c
ISE Hardening <noreply@google.com>
2025-07-16 08:54:33 -0700

    Upgrade dependencies and fix the jasmine test setup
    PiperOrigin-RevId: 783775126
    Change-Id: I36ebe68183dcb2bf0c9a3636839d6cc0249f6914

---
+++ spec/support/jasmine.js
@@ -0,0 +1,11 @@
+require('ts-node/register');
+module.exports = {
+  "spec_dir": "test",
+  "spec_files": [
+    "**/*[sS]pec.ts"
+  ],
+  "helpers": [
+  ],
+  "stopSpecOnExpectationFailure": false,
+  "random": false
+};
