e7e84ef93e1da76b03b85187fe0497ed0e920833
Guillaume Weghsteen <gweg@google.com>
2025-07-23 09:07:15 -0700

    Add the license to additional files. Also update the license URL to be https.
    PiperOrigin-RevId: 786304603
    Change-Id: I313bc0c8bf2d9b259233e95ac335c97d33dc23ab

--- common/third_party/tsetse/util/confidence.ts
+++ common/third_party/tsetse/util/confidence.ts
@@ -1,3 +1,17 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 /**
  * @fileoverview Captures the level of confidence in a failure. Certain failures
  * are more likely to be false positives than others, for instance when a match
