69db59bb78a099904131766f7220c84039791d80
Guillaume Weghsteen <gweg@google.com>
2025-08-26 01:20:22 -0700

    No public description
    PiperOrigin-RevId: 799453296
    Change-Id: I910974e1dc326b5d1872ff5b3d866a84fca5c6c1

--- common/third_party/tsetse/util/pattern_engines/match.ts
+++ common/third_party/tsetse/util/pattern_engines/match.ts
@@ -54,68 +54,37 @@ export function isLegacyMatch(
  */
 export enum TypeMatchConfidence {
   /** Non applicable to this match. */
-  NA,
+  NA = 'NA',
   /**
    * Type matches with the legacy type matching logic, e.g. the
    * PropertyMatcher without useTypedPropertyMatching enabled, does not use
    * isAssignableTo() to compare types.
    */
-  LEGACY_MATCH,
+  LEGACY_MATCH = 'LEGACY_MATCH',
   /**
    * Type does not match with the legacy type matching logic.
    */
-  LEGACY_NO_MATCH,
+  LEGACY_NO_MATCH = 'LEGACY_NO_MATCH',
   /**
    * Type is exactly the matcher's specification
    */
-  EXACT,
+  EXACT = 'EXACT',
   /**
    * Type extends the type of the matcher
    */
-  EXTENDS,
+  EXTENDS = 'EXTENDS',
   /**
    * Matcher's type extends the type of the node
    */
-  PARENT,
+  PARENT = 'PARENT',
   /**
    * Type is any or unknown.
    */
-  ANY_UNKNOWN,
+  ANY_UNKNOWN = 'ANY_UNKNOWN',
   /**
    * Type is unrelated to the matcher
    */
-  UNRELATED,
-}
-
-/**
- * Translate a TypeMatchConfidence enum value to a readable string.
- */
-export function typeMatchConfidenceToString(
-  confidence: TypeMatchConfidence,
-): string {
-  switch (confidence) {
-    case TypeMatchConfidence.NA:
-      return 'NA';
-    case TypeMatchConfidence.LEGACY_MATCH:
-      return 'LEGACY_MATCH';
-    case TypeMatchConfidence.LEGACY_NO_MATCH:
-      return 'LEGACY_NO_MATCH';
-    case TypeMatchConfidence.EXACT:
-      return 'EXACT';
-    case TypeMatchConfidence.EXTENDS:
-      return 'EXTENDS';
-    case TypeMatchConfidence.PARENT:
-      return 'PARENT';
-    case TypeMatchConfidence.ANY_UNKNOWN:
-      return 'ANY_UNKNOWN';
-    case TypeMatchConfidence.UNRELATED:
-      return 'UNRELATED';
-    default:
-      checkExhaustive(
-        confidence,
-        `Found a TypeMatchConfidence that cannot be converted to a string. TypeMatchConfidence case ${confidence} is not covered. This should never happen.`,
-      );
-  }
+  UNRELATED = 'UNRELATED',
 }
 
 /**
@@ -125,37 +94,9 @@ export function typeMatchConfidenceToString(
  */
 export enum NameMatchConfidence {
   /** Non applicable to this match. */
-  NA,
+  NA = 'NA',
   /** Name/property is hardcoded and matches the matcher's specification */
-  EXACT,
+  EXACT = 'EXACT',
   /** Name/property is dynamic (e.g. dynamic array expression access) */
-  DYNAMIC,
-}
-
-/**
- * Translate a NameMatchConfidence enum value to a readable string.
- */
-export function nameMatchConfidenceToString(
-  confidence: NameMatchConfidence,
-): string {
-  switch (confidence) {
-    case NameMatchConfidence.NA:
-      return 'NA';
-    case NameMatchConfidence.EXACT:
-      return 'EXACT';
-    case NameMatchConfidence.DYNAMIC:
-      return 'DYNAMIC';
-    default:
-      checkExhaustive(
-        confidence,
-        `Found a NameMatchConfidence that cannot be converted to a string. NameMatchConfidence case ${confidence} is not covered. This should never happen.`,
-      );
-  }
-}
-
-function checkExhaustive(
-  value: never,
-  msg = `unexpected value ${value}!`,
-): never {
-  throw new Error(msg);
+  DYNAMIC = 'DYNAMIC',
 }
