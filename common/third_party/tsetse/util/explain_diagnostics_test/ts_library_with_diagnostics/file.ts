91f538cac10f7dd4395c00a7ca55a2f989927026
Guillaume Weghsteen <gweg@google.com>
2025-12-24 01:26:47 -0800

    No public description
    PiperOrigin-RevId: 848474331
    Change-Id: I3828af5729ca8eb4c6eed2f520e1a426ef6ab7ea

---
+++ common/third_party/tsetse/util/explain_diagnostics_test/ts_library_with_diagnostics/file.ts
@@ -0,0 +1,29 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+class Foo {
+  src = '';
+}
+
+function foo(): void {
+  const div = document.createElement('div');
+  div.innerText = 'hello';
+  div.innerHTML = 'hello';
+  (div as HTMLElement).innerHTML = 'hello';
+  // tslint:disable-next-line:no-any
+  (div as any).innerHTML = 'hello';
+  new Foo().src = 'hello';
+  // tslint:disable-next-line:no-any
+  (div as any as Foo).src = 'hello';
+}
